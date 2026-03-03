1823b80f464e537cddbfe69bf90979afae5fc8ec
ndk0410 <khangnguyen0414@gmail.com>
2026-03-04 01:41:14 +0700

    Update Firebase config with real keys

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,23 @@
-// firebase-config.js
-// Firebase configuration for MusicFlow
+// Nhập các hàm cần thiết từ các SDK cần thiết
+import { initializeApp } from "firebase/app" ;   
+import { getAnalytics } from "firebase/analytics" ;   
+// Việc cần làm: Thêm SDK cho các sản phẩm Firebase mà bạn muốn sử dụng
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace these with your actual Firebase project config
-const firebaseConfig = {
-    // You will need to create a project at https://console.firebase.google.com/
-    // and copy your Firebase SDK snippet here.
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+// Cấu hình Firebase của ứng dụng web của bạn
+// Đối với Firebase JS SDK phiên bản 7.20.0 trở lên, measurementId là tùy chọn.
+const firebaseConfig = { 
+  apiKey : "AIzaSyAlIi5GMCitFd0jjzg9-mCIxVaPZTYmwTI" , 
+  authDomain : "cocatube-71263.firebaseapp.com" , 
+  projectId : "cocatube-71263" , 
+  storageBucket : "cocatube-71263.firebasestorage.app" , 
+  messagingSenderId : "327359182773" , 
+  appId : "1:327359182773:web:70cefcc72d805505f9cbfc" , 
+  measurementId : "G-XJ4K503CL6" 
 };
+
+// Khởi tạo Firebase
+
 
 // Initialize Firebase
 if (!firebase.apps.length) {
@@ -29,7 +35,8 @@ db.enablePersistence().catch((err) => {
         console.warn('The current browser does not support all of the features required to enable persistence');
     }
 });
-
+const app = initializeApp ( firebaseConfig );
+const analytics = getAnalytics ( app );
 // Export to window for global access
 window.firebaseAuth = auth;
 window.firebaseDb = db;
